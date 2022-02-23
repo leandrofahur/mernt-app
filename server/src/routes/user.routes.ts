8992316e24e349553100f48aa8c6c7c357cafbb5
Leandro Machado <fahurleandro@gmail.com>
2022-02-23 13:57:16 -0800

    clean start

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,7 +1,7 @@
-import { AuthenticateUserController } from '@controllers/User/AuthenticateUserController';
-import { CreateUserController } from '@controllers/User/CreateUserController';
-import { FindAllUsersController } from '@controllers/User/FindUsersController';
-import { UpdateUserController } from '@controllers/User/UpdateUserController';
+// import { AuthenticateUserController } from '@controllers/User/AuthenticateUserController';
+// import { CreateUserController } from '@controllers/User/CreateUserController';
+// import { FindAllUsersController } from '@controllers/User/FindUsersController';
+// import { UpdateUserController } from '@controllers/User/UpdateUserController';
 import { Router } from 'express';
 const router = Router();
 
@@ -10,31 +10,31 @@ const router = Router();
  * @desc    login route for the user
  * @acess   public
  */
-const authenticateUserController = new AuthenticateUserController();
-router.get('/api/login', authenticateUserController.handle);
+// const authenticateUserController = new AuthenticateUserController();
+// router.get('/api/login', authenticateUserController.handle);
 
 /**
  * @route   GET /api/users
  * @desc    fetch all users
  * @acess   public
  */
-const findAllUsersController = new FindAllUsersController();
-router.get('/api/users', findAllUsersController.handle);
+// const findAllUsersController = new FindAllUsersController();
+// router.get('/api/users', findAllUsersController.handle);
 
 /**
  * @route   POST /api/users
  * @desc    create the user
  * @acess   public
  */
-const createUserController = new CreateUserController();
-router.post('/api/users', createUserController.handle);
+// const createUserController = new CreateUserController();
+// router.post('/api/users', createUserController.handle);
 
 /**
  * @route   PUT /api/users/:id
  * @desc    update the user profile by id
  * @acess   public
  */
-const updateUserController = new UpdateUserController();
-router.put('/api/users/:id', updateUserController.handle);
+// const updateUserController = new UpdateUserController();
+// router.put('/api/users/:id', updateUserController.handle);
 
 export { router as userRouter };
